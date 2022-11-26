8e13d5f48c0106b346b0798c811d55fb39b092f5
hsj._.06 <gtw030488@gmail.com>
2022-11-26 13:51:12 +0900

    //

--- untitled/lib/main.dart
+++ untitled/lib/main.dart
@@ -26,10 +26,33 @@ class MyApp extends StatelessWidget {
 }
 
 class RandomWordState extends StatefulWidget {
-  const RandomWordState({Key? key}) : super(key: key);
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
 
   @override
   State<RandomWordState> createState() => _RandomWordStateState();
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
 }
 
 class _RandomWordStateState extends State<RandomWordState> {
